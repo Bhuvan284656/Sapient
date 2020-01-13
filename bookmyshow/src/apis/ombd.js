a07c30d3827edd0242496ff3f3f74842a90bfb37
Bhuvan284656 <47515384+Bhuvan284656@users.noreply.github.com>
2020-01-13 17:29:15 +0530

    warning fix

--- bookmyshow/src/apis/ombd.js
+++ bookmyshow/src/apis/ombd.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://www.omdbapi.com/?page=2&apikey=8d9f24de";
+const url = "https://www.omdbapi.com/?page=2&apikey=8d9f24de";
 
 //Get Movie List by searching Key word
 export const movieList = (search="Batman") => axios.get(`${url}&s=${search}`);
